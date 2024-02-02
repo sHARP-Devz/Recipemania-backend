3cf0020c47217199ee148c4692ba9298e3345ac9
DamiAdeeso <damilolaadeeso@gmail.com>
2024-02-02 23:53:16 +0100

    add recipe controller

--- Backend/src/main/java/com/SharpDevs/Recipe/Mania/Controller/RecipeController.java
+++ Backend/src/main/java/com/SharpDevs/Recipe/Mania/Controller/RecipeController.java
@@ -1,9 +1,20 @@
 package com.SharpDevs.Recipe.Mania.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SharpDevs.Recipe.Mania.Service.RecipeService;
+import com.SharpDevs.Recipe.Mania.domain.DTO.RecipeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/user/recipe")
 public class RecipeController {
+    private final RecipeService recipeService;
+    @PostMapping(path = "/recipe")
+    public ResponseEntity addRecipe(@RequestBody()RecipeDto recipeDto){
+        return new ResponseEntity(recipeService.addRecipe(recipeDto), HttpStatus.CREATED);
+    }
 }
