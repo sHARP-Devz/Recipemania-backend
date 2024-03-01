47d33ba6879193c3fbcc892d0dd02515451ac8ee
DamiAdeeso <damilolaadeeso@gmail.com>
2024-03-01 15:11:41 +0100

    state

--- src/main/java/com/SharpDevs/Recipe/Mania/Controller/UserController.java
+++ src/main/java/com/SharpDevs/Recipe/Mania/Controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user/")
-@CrossOrigin
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService ;
