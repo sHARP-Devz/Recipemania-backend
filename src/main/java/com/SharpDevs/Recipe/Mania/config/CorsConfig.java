aa1b20ace07375f51a5e7b63dd7f13c76f1ecaa0
DamiAdeeso <damilolaadeeso@gmail.com>
2024-03-01 11:18:46 +0100

    set origin

--- src/main/java/com/SharpDevs/Recipe/Mania/config/CorsConfig.java
+++ src/main/java/com/SharpDevs/Recipe/Mania/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
 public void addCorsMappings(CorsRegistry registry){
     registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("*")
             .allowedHeaders("GET","POST","PUT","DELETE","PATCH")
             .allowedHeaders("*")
             .allowCredentials(true)
