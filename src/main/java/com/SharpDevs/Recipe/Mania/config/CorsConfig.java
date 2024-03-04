620073669eae111e5db2dc5cbfb1b11dd423d610
DamiAdeeso <damilolaadeeso@gmail.com>
2024-03-04 11:50:08 +0100

    accidental

--- src/main/java/com/SharpDevs/Recipe/Mania/config/CorsConfig.java
+++ src/main/java/com/SharpDevs/Recipe/Mania/config/CorsConfig.java
@@ -1,21 +1,27 @@
 package com.SharpDevs.Recipe.Mania.config;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ControllerAdvice
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+@Configuration
+public class CorsConfig {
 
-    @Override
-public void addCorsMappings(CorsRegistry registry){
-    registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedHeaders("GET","POST","PUT","DELETE","PATCH")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
+    @Configuration
+    public class WebMvcConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*");
+        }
     }
 }
